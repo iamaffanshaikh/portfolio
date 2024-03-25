7774f8372c7aa1c1d96b8116c37468b18b1f0e06
iamaffanshaikh <iamaffanshaik71@gmail.com>
2024-03-25 14:14:35 +0530

    designation section in progress

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Header from './components/header/Header'
 import Namelabel from "./components/name_label/Namelabel"
-import About from "../src/components/about/About"
+import About from "../src/components/about/About.jsx"
+import Designations from './components/designations/Designations.jsx'
+import '../src/App.css'
 
 const App = () => {
   return (
     <>
+    <div className='main'>
+
       <Navbar/>
       <Header/>
       <Namelabel/>
       <About/>
+      <Designations/>
+
+    </div>
     </>
   )
 }
---
+++ src/components/designations/Designations.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../designations/designation.css'
+
+const Designations = () => {
+  return (
+    <div className="designationcontainer">
+
+        <div className="headingofdsgn">
+            <h4>usually known for</h4>
+        </div>
+
+        <div className="edesignationcon">
+
+            <div className="exactdesignations">
+                <h2> <span>Brand Indentity</span> <br/> Designing </h2>
+            </div>
+
+            <div className="exactdesignations">
+                <h2> <span>Brand Indentity</span> <br/> Designing </h2>
+            </div>
+
+            <div className="exactdesignations">
+                <h2> <span>Brand Indentity</span> <br/> Designing </h2>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Designations
